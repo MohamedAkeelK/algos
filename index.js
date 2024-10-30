5d60f405039cf6f141d198a299307a1a63b23bc1
Mohamed Akeel Khan <Mohamedakeelkhan@gmail.com>
2024-10-30 18:05:34 -0400

    started first algo

---
+++ index.js
@@ -0,0 +1,10 @@
+function same(arr1, arr2) {
+  // if arrays are not same length then return false 
+
+  // loop through first array
+   // find the correct index in array 2 that is the square of the index in array 1
+   // if not found return false
+   // if found then remove that number from array 2
+  
+   // return true
+}
